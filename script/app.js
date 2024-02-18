7c80a9404f3dae16f504f1e7a1122273ab2d28d0
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-02-18 18:55:18 +0600

    next button killed

--- script/app.js
+++ script/app.js
@@ -1,13 +1,17 @@
 const allTicket = document.querySelectorAll('#ticket');
 let ticketCount = 0;
-console.log(allTicket);
+const btnClick = [];
 
 for (const ticket of allTicket) {
     ticket.addEventListener('click', function () {
         ticketCount++;
+        btnClick.push(ticketCount);
+
         if (ticketCount > 4) {
             return 0;
         }
+
+
 
         if (ticketCount === 4) {
             document.getElementById('coupon-field').removeAttribute('disabled');
@@ -27,11 +31,11 @@ for (const ticket of allTicket) {
         p1.innerText = 'Economy';
         const p2 = document.createElement('p');
         p2.innerText = 550;
-  
+
         li.appendChild(p);
         li.appendChild(p1);
         li.appendChild(p2);
-        
+
         ticketList.appendChild(li);
 
 
@@ -42,13 +46,121 @@ for (const ticket of allTicket) {
         let ticketLeft = parseInt(ticketLeftText);
         ticketLeftTextElement.innerText = ticketLeft - 1;
 
+        //increase the total price
+        const currentTotalPriceTextElement = document.getElementById('total-price');
+        const currentTotalPriceText = currentTotalPriceTextElement.innerText;
+        let currentTotalPrice = parseInt(currentTotalPriceText);
+
+
+        currentTotalPrice += 550;
+
+        currentTotalPriceTextElement.innerText = currentTotalPrice;
+
+
+
+        //increase the grand total
+        const grandTotalTextElement = document.getElementById('grand-total');
+        grandTotalTextElement.innerText = currentTotalPrice;
+
 
         //give style for sold ticket
         ticket.style.backgroundColor = '#1DD100';
         ticket.style.color = 'white';
         //disable the ticket after sold
         ticket.setAttribute('disabled', "");
+
+
+        updateButtonState();
     })
 }
 
+const phoneNumberInput = document.getElementById('phone-number');
+phoneNumberInput.addEventListener('input', function () {
+    updateButtonState();
+});
 
+
+function updateButtonState() {
+    const inputNumberElement = document.getElementById('phone-number');
+    const number = inputNumberElement.value;
+    const nextButton = document.getElementById('next-button');
+
+    if (number.length > 0 && btnClick.length > 0) {
+        nextButton.removeAttribute('disabled');
+        nextButton.style.opacity = '100%';
+    } else {
+        nextButton.setAttribute('disabled', 'disabled');
+        nextButton.style.opacity = '50%';
+    }
+}
+
+updateButtonState();
+
+
+
+//apply button function 
+function applyBonus() {
+    const inputCodeElement = document.getElementById('coupon-field');
+    const inputCode = inputCodeElement.value;
+
+    //current total price
+    const currentTotalPriceTextElement = document.getElementById('total-price');
+    const currentTotalPriceText = currentTotalPriceTextElement.innerText;
+    let currentTotalPrice = parseInt(currentTotalPriceText);
+
+    if (inputCode === 'NEW15') {
+
+        const discount = (currentTotalPrice * 15) / 100;
+        const grandTotal = currentTotalPrice - discount;
+        const grandTotalTextElement = document.getElementById('grand-total');
+        grandTotalTextElement.innerText = grandTotal;
+
+
+        const priceName = document.createElement('h1');
+        priceName.innerText = 'Discount Price';
+        const dPrice = document.createElement('p');
+        dPrice.innerText = 'BDT' + discount;
+
+
+        document.getElementById('bonus-panel').appendChild(priceName);
+        document.getElementById('bonus-panel').appendChild(dPrice);
+
+
+        document.getElementById('coupon-div').classList.add('hidden');
+
+    }
+
+    else if (inputCode === 'Couple 20') {
+
+        const discount = (currentTotalPrice * 20) / 100;
+        const grandTotal = currentTotalPrice - discount;
+        const grandTotalTextElement = document.getElementById('grand-total');
+        grandTotalTextElement.innerText = grandTotal;
+
+
+        const priceName = document.createElement('h1');
+        priceName.innerText = 'Discount Price';
+        const dPrice = document.createElement('p');
+        dPrice.innerText = 'BDT' + discount;
+
+
+        document.getElementById('bonus-panel').appendChild(priceName);
+        document.getElementById('bonus-panel').appendChild(dPrice);
+
+
+        document.getElementById('coupon-div').classList.add('hidden');
+
+    }
+
+    else {
+        alert('please enter the valid coupon')
+    }
+}
+
+
+
+
+function modalPge() {
+    document.getElementById('modal-page').classList.remove('hidden');
+    document.getElementById('modal-page').classList.add('static');
+}
